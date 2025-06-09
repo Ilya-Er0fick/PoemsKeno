ceabe40ccb8967bc3548d0d80e49c64aac6d6389
Ilya-Er0fick <ilyaerofick@gmail.com>
2025-06-09 12:42:28 +0300

    Add Poems.cpp & Poems.hpp

---
+++ Poems.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include "Poems.hpp"
+
+int main(){
+    int i = 0;
+    for (i; i < 15 ;  i++) {
+
+    }
+}
---
+++ Poems.hpp
@@ -0,0 +1,13 @@
+#ifndef POEMS_HPP
+#define POEMS_HPP
+
+#include <iostream>
+#include <vector>
+
+namespace Poems{
+    struct numString{
+        int i;
+    };
+}
+
+#endif
