9013dbfa8d33e5e47ca910e8c9ae2344e5450a04
Ilya-Er0fick <ilyaerofick@gmail.com>
2025-06-10 18:56:50 +0300

    v1.0 Poems.cpp add function open files in order

--- Poems.cpp
+++ Poems.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <string>
 #include "Poems.hpp"
 
-int main(){
-    int i = 0;
-    for (i; i < 15 ;  i++) {
 
+int main(){
+    for(int num = 1; num < 11; num++){
+        std::string filename = std::to_string(num) + ".txt";
+        std::ifstream file(filename);
+
+        if (!file.is_open()){
+            std::cerr << "Error open file: " << filename << std::endl;
+            break; //because withou something file we didn't have normal text poem
+        }
     }
 }
